216e05050f35c223ac26133900dc16b20eef163f
Eden Mesfun <emesfun@gmail.com>
2018-01-29 17:45:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
         }
      }
 		#  programmer_hash[:dennis_ritchie][:languages]
-		programmer_hash[:dennis_ritchie][:languages].map { |languages|}.join()
+		programmer_hash[:dennis_ritchie][:languages].map { |languages|}.join(" ")
 end
 		 dennis_ritchies_language
 
